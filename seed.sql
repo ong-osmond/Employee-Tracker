625d76d34fd52a4943b8d60726cec3d04f184838
ong_osmond <ong.osmond@yahoo.com>
2020-07-31 06:44:25 +0800

    Updated setting up the schema scripts.

--- seed.sql
+++ seed.sql
@@ -43,17 +43,15 @@ CREATE TABLE employee (
 ) ENGINE=INNODB;
 
 INSERT INTO department (name)
-VALUES ("Gondor"), ("Rohan"), ("Mordor");
+VALUES ("Gondor"), ("Rivendell"), ("Hobbiton"), ("Wizards");
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-("Ranger",99.99,(select id from department where name = "Gondor")),
-("Archer",89.99,(select id from department where name = "Gondor")),
-("Axeman",89.99,(select id from department where name = "Gondor")),
-("Wizard",99.99,(select id from department where name = "Rohan")),
-("Hobbit",79.99,(select id from department where name = "Rohan")),
-("Ring-bearer",99.99,(select id from department where name = "Mordor")),
-("Hobbit",89.99,(select id from department where name = "Mordor"))
+("Ranger",100,(select id from department where name = "Gondor")),
+("Archer",90,(select id from department where name = "Rivendell")),
+("Wizard",100,(select id from department where name = "Wizards")),
+("Hobbit",80,(select id from department where name = "Hobbiton")),
+("Ring-bearer",90,(select id from department where name = "Hobbiton"))
 ;
 
 select role.id, role.title, role.salary, role.department_id, department.name
@@ -62,15 +60,15 @@ join department on role.department_id = department.id
 order by role.title
 ;
 
-
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
 ("Aragorn","Son of Arathorn",(select id from role where title = "Ranger"),null),
 ("Frodo","Baggins",(select id from role where title = "Ring-bearer"),null),
-("Gandalf","The White",(select id from role where title = "Wizard"),null)
+("Gandalf","The White",(select id from role where title = "Wizard"),null),
+("Legolas","Greenleaf",(select id from role where title = "Archer"),null)
 ;
 
-select employee.id, employee.first_name, employee.last_name, role.title, employee.manager_id, department.name
+select employee.id, employee.first_name, employee.last_name, role.title, employee.manager_id, department.name department
 from employee
 join role on employee.role_id = role.id
 join department on role.department_id = department.id
